b343924056bfb8479b7136f0784e515c7a543cf1
Col Harris <col.w.harris@gmail.com>
2017-03-06 12:14:12 +0800

    Switch to lex-sdk

--- handler.js
+++ handler.js
@@ -2,38 +2,30 @@
 
 var util = require('util')
 var Robin = require('robin-js-sdk')
+var Lex = require('lex-sdk')
 
 function inspect(obj) {
   return util.inspect(obj, false, null)
 }
 
-// This is a hack. We should create a proper library that handles
-// the formatting for valid Lax responses.
-function responseWithContent(content) {
-    return {
-        sessionAttributes: {},
-        dialogAction: {
-            type: "Close",
-            fulfillmentState: "Fulfilled",
-            message: {
-                contentType: "PlainText",
-                content: content
-            }
-        }
-    }
+var handlers = {
+  'BookMeetingRoom': function() {
+    var robin = new Robin(process.env.ROBIN_API_TOKEN)
+    var organizationId = process.env.ROBIN_ORGANIZATION_ID
+    var locationId = process.env.ROBIN_LOCATION_ID
+
+    robin.api.locations.spaces.get(locationId).then(function(response) {
+      var data = response.getData()
+      console.log("Spaces = " + inspect(data))
+    })
+
+    this.emit(':tell', "Booking meeting rooms is not yet support. Please try again tomorrow.");
+  }
 }
 
 module.exports.bookMeetingRoom = (event, context, callback) => {
   console.log("Event = " + inspect(event))
-
-  var robin = new Robin(process.env.ROBIN_API_TOKEN)
-  var organizationId = process.env.ROBIN_ORGANIZATION_ID
-  var locationId = process.env.ROBIN_LOCATION_ID
-
-  robin.api.locations.spaces.get(locationId).then(function(response) {
-    var data = response.getData()
-    console.log("Spaces = " + inspect(data))
-  })
-
-  callback(null, responseWithContent("Booking meeting rooms is not yet support. Please try again tomorrow."))
+  var lex = Lex.handler(event, context)
+  lex.registerHandlers(handlers)
+  lex.execute()
 };
