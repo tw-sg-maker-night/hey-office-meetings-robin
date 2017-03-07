b5380f815244627484aa2387935b63f1bdc42295
Colin Harris <col.w.harris@gmail.com>
2017-03-07 10:11:13 +0800

    Refactoring

--- handler.js
+++ handler.js
@@ -8,25 +8,60 @@ function inspect(obj) {
   return util.inspect(obj, false, null)
 }
 
-const VALID_ROOMS = ['Amoy']
+const VALID_ROOMS = [{name: 'Amoy', id: 23207}]
+
+function roomForName(name) {
+  return VALID_ROOMS.filter(room => {
+    return room.name == name
+  })[0] || null
+}
+
+function validateRoom(handler) {
+  var requestedRoomName = handler.slots.MeetingRoom
+  var room = roomForName(requestedRoomName)
+  if (room) {
+    // TODO: check availability
+    handler.attributes.room = room
+    return true
+  } else {
+    delete handler.attributes.room
+    handler.slots.MeetingRoom = null
+    handler.emit(':elicit', 'MeetingRoom', `${requestedRoomName} is not a valid meeting room. Which meeting room would you like?`)
+    return false
+  }
+}
 
 var handlers = {
   'BookMeetingRoom.Dialog': function() {
-    var requestedRoom = this.slots.MeetingRoom
-    console.log("requestedRoom:", requestedRoom)
-    var validRoom = VALID_ROOMS.filter(room => {
-      return room == requestedRoom
-    })[0] || null
-    console.log("validRoom:", validRoom)
-    if (validRoom) {
-      this.attributes.MeetingRoom = validRoom
+    if (validateRoom(this)) {
       this.emit(':delegate')
-    } else {
-      this.emit(':elicit', 'MeetingRoom', `${requestedRoom} is not a valid meeting room. Which meeting room would you like?`)
     }
   },
 
   'BookMeetingRoom.Fulfillment': function() {
+    var robin = new Robin(process.env.ROBIN_API_TOKEN)
+    var organizationId = process.env.ROBIN_ORGANIZATION_ID
+    var locationId = process.env.ROBIN_LOCATION_ID
+
+    robin.api.spaces.events.post(this.attributes.room.id, {
+      title: "Test Event",
+      start: {
+        date_time: '',
+        time_zone: ''
+      },
+      end: {
+        date_time: '',
+        time_zone: ''
+      },
+      description: '',
+      visibility: '',
+      recurrence: '',
+      // invitees: [{email: '', name: ''}]
+    }).then(function(response) {
+      var data = response.getData()
+      console.log("Spaces = " + inspect(data))
+    })
+
     this.emit(':tell', "Booking meeting rooms is not yet support. Please try again tomorrow.");
   }
 
--- spec/handler_spec.js
+++ spec/handler_spec.js
@@ -1,27 +1,46 @@
 const nock = require('nock');
 const handler = require('../handler');
 
-function testEvent(intentName = 'TestIntent', invocationSource = 'FulfillmentCodeHook', sessionAttributes = {}, slots = {}) {
-    return {
-      sessionAttributes: sessionAttributes,
-      invocationSource: invocationSource,
-      currentIntent: {
-        name: intentName,
-        slots: slots
-      }
+function testEvent(intentName, invocationSource, sessionAttributes, slots) {
+  intentName = intentName || 'TestIntent'
+  invocationSource = invocationSource || 'FulfillmentCodeHook'
+  sessionAttributes = sessionAttributes || {}
+  slots = slots || {}
+  return {
+    sessionAttributes: sessionAttributes,
+    invocationSource: invocationSource,
+    currentIntent: {
+      name: intentName,
+      slots: slots
     }
+  }
 }
 
 describe('BookMeetingRoom Handler', () => {
 
   describe('Setting meeting room', () => {
 
-    it('should Delegate', (done) => {
+    it('should delegate for a valid meeting room', (done) => {
       var event = testEvent('BookMeetingRoom', 'DialogCodeHook', {}, {MeetingRoom: "Amoy"})
       handler.bookMeetingRoom(event, {
         succeed: function(response) {
-          console.log('Response = ', response)
-          expect(response.dialogAction.type).toBe('Delegate')
+          expect(response.dialogAction.type).toEqual('Delegate')
+          expect(response.dialogAction.slots.MeetingRoom).toEqual('Amoy')
+          expect(response.sessionAttributes.room).toEqual({name: 'Amoy', id: 23207})
+          done();
+        }
+      })
+    });
+
+    it('should elicit for a invalid meeting room', (done) => {
+      var event = testEvent('BookMeetingRoom', 'DialogCodeHook', {}, {MeetingRoom: "Invalid"})
+      handler.bookMeetingRoom(event, {
+        succeed: function(response) {
+          expect(response.dialogAction.type).toEqual('ElicitSlot')
+          expect(response.dialogAction.slotToElicit).toEqual('MeetingRoom')
+          expect(response.dialogAction.slots).toEqual({
+            MeetingRoom: null
+          })
           done();
         }
       })
